4173c01125c7904038748144413acc51eb120180
TSimeonov92 <simeonov1992@abv.bg>
2024-10-30 08:30:47 +0200

    Chaning structure

---
+++ s3_buckets/README.md
@@ -0,0 +1,7 @@
+# Managing multiple s3 buckets with Terraform
+
+## In this folder I am showcasing how we can manage s3 buckets with Terraform
+
+The first exaple in the main.tf presents managing multiple s3 buckets with the **count** meta argument.
+
+The second example presents managing multiple s3 buckets in multiple regions with the **for_each** meta argument. 
---
+++ s3_buckets/main.tf
@@ -0,0 +1,43 @@
+# Manage multiple S3 buckets with a single resource
+# Example using count
+
+resource "aws_s3_bucket" "veeam_backup_buckets_eu_west_1" {
+  count         = length(var.s3_bucket_names) //count will be 3
+  bucket        = "${var.s3_bucket_names[count.index]}-eu_west_1"
+  acl           = "private"
+  provider      = aws.eu-west-1
+  force_destroy = true
+
+  tags = {
+    Customer = "${var.s3_bucket_names[count.index]}-eu_west_1"
+  }
+}
+
+# Managing multiple s3 buckets in multiple regions
+# In this example we are using for_each
+
+## Buckets for eu-west-1
+#resource "aws_s3_bucket" "veeam_backup_buckets_eu_west_1" {
+#  for_each      = toset(var.s3_bucket_names)
+#  bucket        = "${each.key}-eu-west-1"
+#  acl           = "private"
+#  provider      = aws.eu-west-1
+#  force_destroy = true
+#
+#  tags = {
+#    Customer = each.key
+#  }
+#}
+#
+## Buckets for eu-west-2
+#resource "aws_s3_bucket" "veeam_backup_buckets_eu_west_2" {
+#  for_each      = toset(var.s3_bucket_names)
+#  bucket        = "${each.key}-eu-west-2"
+#  acl           = "private"
+#  provider      = aws.eu-west-2
+#  force_destroy = true
+#
+#  tags = {
+#    Customer = each.key
+#  }
+#}
